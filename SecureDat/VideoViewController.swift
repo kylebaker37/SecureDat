a9a952a61b8ff041d799cc967c411b4236370c5e
Kyle Baker <kylebaker37@gmail.com>
2017-03-12 20:43:37 -0700

    pause and play

--- SecureDat/VideoViewController.swift
+++ SecureDat/VideoViewController.swift
@@ -30,8 +30,30 @@ class VideoViewController: UIViewController {
         avPlayerLayer.frame = CGRect(x: 25, y: 25, width: screenWidth-50, height: screenWidth-50)
         self.view.layer.addSublayer(avPlayerLayer)
         player.play()
+        
+        let pause = UIButton()
+        pause.frame = (frame: CGRect(x: self.view.frame.size.width - 60, y: self.view.frame.size.width - 60, width: 50, height: 50))
+        pause.backgroundColor = UIColor.red
+        pause.setTitle("Pause", for: .normal)
+        pause.addTarget(self, action: #selector(pause_func), for: .touchUpInside)
+        self.view.addSubview(pause)
+        
+        let play = UIButton()
+        play.frame = (frame: CGRect(x: self.view.frame.size.width - 120, y: self.view.frame.size.width - 60, width: 50, height: 50))
+        play.backgroundColor = UIColor.blue
+        play.setTitle("Play", for: .normal)
+        play.addTarget(self, action: #selector(play_func), for: .touchUpInside)
+        self.view.addSubview(play)
 
         // Do any additional setup after loading the view.
+    }
+    
+    func pause_func(sender: UIButton!) {
+        self.player.pause()
+    }
+    
+    func play_func(sender: UIButton!) {
+        self.player.play()
     }
 
     override func didReceiveMemoryWarning() {
