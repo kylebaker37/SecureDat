8d1e854c0d17632ec5c770d3655c2530e40c2374
Kyle Baker <kylebaker37@gmail.com>
2017-03-14 13:43:49 -0700

    timestamp in frontend

--- SecureDat/VideoTableViewController.swift
+++ SecureDat/VideoTableViewController.swift
@@ -32,13 +32,54 @@ class VideoTableViewController: UIViewController, UITableViewDataSource, UITable
         self.videosTableView.reloadData()
     }
     
+    func convertToTimestamp(filename: String) -> String {
+        if (filename.characters.count != 19) {
+            return "Corrupt File"
+        }
+        let yearIndex = filename.index(filename.startIndex, offsetBy: 4)
+        let year = filename.substring(to: yearIndex)
+        
+        let startMonth = filename.index(filename.startIndex, offsetBy: 4)
+        let endMonth = filename.index(filename.startIndex, offsetBy: 6)
+        let monthRange = startMonth..<endMonth
+        let month = filename.substring(with: monthRange)
+        
+        let startDay = filename.index(filename.startIndex, offsetBy: 6)
+        let endDay = filename.index(filename.startIndex, offsetBy: 8)
+        let dayRange = startDay..<endDay
+        let day = filename.substring(with: dayRange)
+        
+        let startHour = filename.index(filename.startIndex, offsetBy: 9)
+        let endHour = filename.index(filename.startIndex, offsetBy: 11)
+        let hourRange = startHour..<endHour
+        let hour = filename.substring(with: hourRange)
+        
+        let startMinute = filename.index(filename.startIndex, offsetBy: 11)
+        let endMinute = filename.index(filename.startIndex, offsetBy: 13)
+        let minuteRange = startMinute..<endMinute
+        let minute = filename.substring(with: minuteRange)
+        
+        let startSecond = filename.index(filename.startIndex, offsetBy: 13)
+        let endSecond = filename.index(filename.startIndex, offsetBy: 15)
+        let secondRange = startSecond..<endSecond
+        let second = filename.substring(with: secondRange)
+        
+        let firstHalf = month + "-" + day + "-" + year + ", "
+        let secondHalf = hour + ":" + minute + ":" + second
+        let final = firstHalf + secondHalf
+        return final
+        
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.files.count // your number of cell here
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "vidcell", for: indexPath)
-        cell.textLabel?.text = self.files[indexPath.row]
+//        let txt = convertToTimestamp(filename: self.files[indexPath.row])
+        let txt = convertToTimestamp(filename: "20170314T064525.mp4")
+        cell.textLabel?.text = txt
         return cell
     }
     
